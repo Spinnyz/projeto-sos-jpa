19c8c847f673136d6d816689918b82f5b285767d
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-05-27 19:45:37 -0300

    up

--- script.js
+++
@@ -1,8 +0,0 @@
-var comentarios = document.querySelector('.comentarios');
-
-function adicionarComentario(){
-    alert('Comentário adicionado com sucesso!');
-
-}
-
-comentarios.addEventListener('click', adicionarComentario)
